cf46217c404006b480cba7eda0c0c9810bc82b1d
josepardoserpis <josana@alu.iesserpis.org>
2019-10-04 09:56:37 +0200

    Subida de CArticulo,CConsola,CMySql

---
+++ CConsola/CConsola/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CConsola
+{
+    class MainClass
+    {
+        public static void Main(string[] args){
+
+            int entero = readInteger("Introduce un entero: ");
+            Console.WriteLine("entero={0}", entero);
+            int opcion = readOption("Elige opción [012]: ", "012");
+
+        }
+        public static int readInteger(string label) {
+            Console.Write(label);
+            String linea = Console.ReadLine();
+            try {
+                return Int32.Parse(linea);
+            }catch{
+                Console.WriteLine("Formato invalido. Vuelve a introducir. ");
+            }
+
+        }
+
+        public static int readOption(string label, string options){
+            while (true){
+
+
+
+            }
+
+        }
+    }
+}
---
+++ Carticulo/Carticulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Carticulo
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args){
+
+            Console.WriteLine("Accediendo a dbprueba");
+
+            IDbConnection dbConnection = new MySqlConnection("server=localhost;database=dbprueba;user=root;password=sistemas;ssl-mode=none");
+
+            dbConnection.Open();
+
+
+
+        }
+    }
+}
