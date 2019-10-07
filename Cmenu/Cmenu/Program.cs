79c3fd64b837a3add6924bedbb2173c4631f5c2d
josepardoserpis <josana@alu.iesserpis.org>
2019-10-07 10:09:29 +0200

    Actualizado Cconsola y añadido Cmenu

---
+++ Cmenu/Cmenu/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cmenu
+{
+    class MainClass
+    {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+    }
+}
