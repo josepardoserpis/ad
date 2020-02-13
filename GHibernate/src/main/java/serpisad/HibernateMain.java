4efe48f5cf4b2bba4e3e2a34841ddcf8933d5668
josepardoserpis <josana@alu.iesserpis.org>
2020-02-13 09:11:51 +0100

    version basica del proyecto hibernate, sincronizar con lo que tengo hecho en casa

---
+++ GHibernate/src/main/java/serpisad/HibernateMain.java
@@ -0,0 +1,12 @@
+package serpisad;
+
+//import java.time.LocalDateTime;
+//import javax.persistence.*;
+
+public class HibernateMain {
+	public static void main(String[] args) {
+		
+		//EntityManager
+	}
+
+}
---
+++ hibernate/src/main/java/hibernate/articulos.java
@@ -0,0 +1,10 @@
+package hibernate;
+
+public class articulos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
